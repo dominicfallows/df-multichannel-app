79a889544c2f7c1939ded521f204cba45d376b8f
Dominic Fallows <consultant@dominicfallows.uk>
2020-05-20 07:38:29 +0100

    Mobile app WIP

--- packages/mobile/src/App.tsx
+++ packages/mobile/src/App.tsx
@@ -2,8 +2,10 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
  * @format
- * @flow strict-local
  */
 
 import React from 'react';
@@ -15,7 +17,6 @@ import {
   Text,
   StatusBar,
 } from 'react-native';
-
 import {
   Header,
   LearnMoreLinks,
@@ -23,8 +24,13 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import { SvgUri } from 'react-native-svg';
 
-const App: () => React.ReactNode = () => {
+import icon from "@df/multichannel-app-shared/content/assets/icon.svg";
+
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -33,17 +39,22 @@ const App: () => React.ReactNode = () => {
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
           <Header />
-          {typeof global["HermesInternal"] === 'object' && HermesInternal !== null ? null : (
+          {global.HermesInternal == null ? null : (
             <View style={styles.engine}>
               <Text style={styles.footer}>Engine: Hermes</Text>
             </View>
           )}
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
+            <SvgUri
+              width="100%"
+              height="100%"
+              uri={icon}
+            />
               <Text style={styles.sectionTitle}>Step One</Text>
               <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
+                Edit <Text style={styles.highlight}>App.tsx</Text> to change
+                this screen and then come back to see your edits.
               </Text>
             </View>
             <View style={styles.sectionContainer}>
